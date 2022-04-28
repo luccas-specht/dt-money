60af0847714f869ca37e8d646ce614fa487abe52
Luccas Specht <luccasspecht70@gmail.com>
2022-04-28 19:51:24 -0300

    feat: changing font family

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -12,9 +12,9 @@ const fontSize = {
 
 export const GlobalStyle = createGlobalStyle`
   :root {
-    --background: #f0f2f5
+    --background: #f0f2f5;
 
-    --shape: #ffffff
+    --shape: #ffffff;
    
     --red: #e52e4d;
    
@@ -44,6 +44,15 @@ export const GlobalStyle = createGlobalStyle`
   body {
     background-color: var(--background);
     -webkit-font-smoothing: antialiased;
+  }
+  
+  body, input, textarea, button {
+    font-family: 'Poppins', sans-serif;
+    font-weight: 400;
+  }
+
+  h1, h2, h3, h4, h5, h6, strong {
+    font-weight: 600; 
   }
 
   button {
